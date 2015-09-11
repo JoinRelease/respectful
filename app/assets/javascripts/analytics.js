ea6eacbea0c8b116fff5deea91283b0a4f89b042
tai dupree tan <tdupreetan@gmail.com>
2015-09-10 17:06:38 -0700

    adds back calling feature

--- app/assets/javascripts/analytics.js
+++ app/assets/javascripts/analytics.js
@@ -1,6 +1,5 @@
 jQuery(function($){
   $('.quote-req').on('click', function(event){
-    event.preventDefault();
     ga('send', 'event', 'quote-request', 'click', $(this).data('ga-label'));
   });
   $('#quote-submit').on('click', function(event){
@@ -9,7 +8,7 @@ jQuery(function($){
     $(this).submit();
   });
   $('.call-button').on('click', function(event){
-    event.preventDefault();
     ga('send', 'event', 'call-button', 'click', $(this).data('ga-label'));
+
   });
 });
